273e6ff855570f0ae0e3c04229c2bb50ef403611
bolid <bratik.n@yandex.ru>
2022-01-18 15:26:18 +0300

    07.12.21

--- src/klik/klik.java
+++ src/klik/klik.java
@@ -2,6 +2,15 @@ package klik;
 
 public class klik {
     public static void main(String[] args) {
-        System.out.println("аз два три");
-    }
+        int [][]mass =  new int [][]{
+                {1},                                                                 //0
+                {2, 3},                                                           //1
+                {5, 6, 7},                                                    //2
+                {8, 9, 10, 11},                                            //3
+
+        };
+
+        System.out.println(mass[3][2]);
+
+}
 }
