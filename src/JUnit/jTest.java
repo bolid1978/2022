39f42c35077ecc17d18ad7fe2b89bc5965ef6922
bolid <bratik.n@yandex.ru>
2022-02-07 16:14:24 +0300

    07.12.21

---
+++ src/JUnit/jTest.java
@@ -0,0 +1,39 @@
+package JUnit;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class jTest {
+    @BeforeEach
+    public void setUp() throws Exception {
+        System.out.println("Старт второго теста");
+//            user1 = new Persons("Марина", 34, Sex.FEMALE);
+//            user2= new Persons("Евгений", 35, Sex.MALE);
+//            user3 = new Persons("Алина", 7, Sex.FEMALE);
+    }
+    @Test
+    void testGetAllUsers() {
+
+
+
+
+            Persons user1 =  new Persons("Маша", 23, Sex.FEMALE);
+        Persons user2 =  new Persons("Cаша", 23, Sex.MALE);
+        Persons user3 =  new Persons("Lаша", 23, Sex.FEMALE);
+
+        List<Persons> expectedMale = Persons.getAllUsers(Sex.MALE);
+        List<Persons> expectedFemale = Persons.getAllUsers(Sex.FEMALE);
+        List<Persons> actual = new ArrayList<>();
+        actual.add(user2);
+        List<Persons> actual2 = new ArrayList<>();
+        actual2.add(user1);
+        actual2.add(user3);
+        Assert.assertEquals(expectedMale,actual );
+        Assert.assertEquals(expectedFemale,actual2 );
+
+    }
+}
