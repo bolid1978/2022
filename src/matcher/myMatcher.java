4e913b903777db84c80e8aeea5b6fabcc4cc098f
bolid <bratik.n@yandex.ru>
2022-03-01 13:50:34 +0300

    07.12.21 + matcher+ lib

--- src/matcher/myMatcher.java
+++ src/matcher/myMatcher.java
@@ -7,9 +7,11 @@ public class myMatcher {
     //https://javarush.ru/groups/posts/regulyarnye-vyrazheniya-v-java
     public static void main(String[] args) {
         String str = "get ip for user = \"Eduard Petrovich Morozko\" and date between \"11.12.2013 0:00:00\" and \"03.01.2014 23:59:59\".";
+        str = "2345 595";
         System.out.println(str);
         Pattern pattern = Pattern.compile("\".+?\"");
         Matcher matcher = pattern.matcher(str);
+        System.out.println(str.matches("\\d+ \\d+"));
         while (matcher.find()) {
             System.out.println(matcher.group());
            // System.out.println(str.substring(matcher.start(), matcher.end()));
