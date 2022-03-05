512d582c6cf66a2a5df01971009e54174fdb9cbc
bolid <bratik.n@yandex.ru>
2022-03-05 11:11:17 +0300

    07.12.21 loadfile

---
+++ src/loadFile/Load.java
@@ -0,0 +1,17 @@
+package loadFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import javax.mail.* ;
+import javax.mail.internet.*;
+
+public class Load {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://avatars.mds.yandex.net/i?id=4611452e1b440fd146e1d2409432cc38-4831580-images-thumbs&n=13");
+        InputStream inputStream = url.openStream();
+        Files.copy(inputStream, new File("c:\\1\\google.png").toPath());
+    }
+}
