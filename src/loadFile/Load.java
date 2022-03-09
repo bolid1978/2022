5f2a487c605d6b30ebf05e45a2c697fc0a368978
bolid <bratik.n@yandex.ru>
2022-03-09 11:29:34 +0300

    07.12.21 loadfile

--- src/loadFile/Load.java
+++ src/loadFile/Load.java
@@ -13,5 +13,7 @@ public class Load {
         URL url = new URL("https://avatars.mds.yandex.net/i?id=4611452e1b440fd146e1d2409432cc38-4831580-images-thumbs&n=13");
         InputStream inputStream = url.openStream();
         Files.copy(inputStream, new File("c:\\1\\google.png").toPath());
+        System.out.println();
+
     }
 }
