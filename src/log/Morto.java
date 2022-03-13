d9ead9acf168258237ced6be09551604fbc91201
bolid1978 <95186968+bolid1978@users.noreply.github.com>
2022-03-13 16:42:47 +0300

    Loger

--- src/log/Morto.java
+++ src/log/Morto.java
@@ -2,12 +2,18 @@ package log;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
 public class Morto {
     public static final Logger LOGGER = LoggerFactory.getLogger(Morto.class);
 
     public static void main(String[] args) {
-        DOMConfigurator.configure("C:\\Users\\bolid\\IdeaProjects\\Test\\src\\log\\2.xml");
-        LOGGER.error("Начало работы программы!!!");
+        ArrayList<Integer> arrayList = new ArrayList();
+        arrayList.add(12);
+        DOMConfigurator.configure("C:\\Users\\sergey\\IdeaProjects\\2022\\src\\log\\2.xml");
+        LOGGER.info("Начало работы программы!!!");
+        LOGGER.info(arrayList.toString(),arrayList);
 
         try {
             LOGGER.warn("Внимание! Программа пытается разделить одно число на другое");
