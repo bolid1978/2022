c39652d557f4358cfdad7a97ebc8d6b5d745b4c5
bolid <bratik.n@yandex.ru>
2022-03-21 13:16:06 +0300

    port,ip for client main

--- src/MyClient/ClientServer.java
+++ src/MyClient/ClientServer.java
@@ -9,9 +9,14 @@ import java.net.Socket;
 
 public class ClientServer {
     public static final Logger LOGGERSocket =  LoggerFactory.getLogger(ClientServer.class);
-    public static void main(String[] args) {
-        DOMConfigurator.configure("C:\\Users\\bolid\\IdeaProjects\\Test\\src\\MyClient\\properties\\2.xml");
 
+    public static void main(String[] args) {
+        DOMConfigurator.configure("C:\\Users\\bolid\\IdeaProjects\\Test\\src\\MyClient\\properties\\loger.xml");
+
+        ConsolHelper.write("Введите ip адресс");
+        String ip = ConsolHelper.stringIp();
+        ConsolHelper.write("Введите порт");
+        int  port = ConsolHelper.intPort();
 
     }
 }
