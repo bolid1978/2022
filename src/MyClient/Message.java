a28bb4231ad8939c47bece8d25fe99809ba12253
bolid <bratik.n@yandex.ru>
2022-03-23 09:38:47 +0300

    port,ip for client main + Connect + run + Message

--- src/MyClient/Message.java
+++ src/MyClient/Message.java
@@ -1,4 +1,28 @@
 package MyClient;
 
+import java.awt.*;
+
+//----------------тут собствено формат сообщения всё что хотим передать
 public class Message {
+    String string;
+    TypeMesange typeMesange;
+    static public int countId;
+
+    public Message(String string, TypeMesange typeMesange) {
+        this.string = string;
+        this.typeMesange = typeMesange;
+        countId ++;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public TypeMesange getTypeMesange() {
+        return typeMesange;
+    }
+
+    public static int getCountId() {
+        return countId;
+    }
 }
