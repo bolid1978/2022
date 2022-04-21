74012449edc8c9f9d55b45f315bc5c3fce5791ad
bolid <bratik.n@yandex.ru>
2022-04-21 10:50:35 +0300

    swing

---
+++ src/sw/sw.java
@@ -0,0 +1,12 @@
+package sw;
+
+import javax.swing.*;
+
+public class sw {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+
+        jf.setSize(500,600);
+        jf.setVisible(true);
+    }
+}
