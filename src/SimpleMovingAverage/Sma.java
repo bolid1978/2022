344f8177c042f770de719056293bf806c586b924
bolid1978 <95186968+bolid1978@users.noreply.github.com>
2022-06-21 21:15:52 +0300

    SMA

---
+++ src/SimpleMovingAverage/Sma.java
@@ -0,0 +1,9 @@
+package SimpleMovingAverage;
+
+//https://andrewlock.net/creating-a-simple-moving-average-calculator-in-csharp-1-a-simple-moving-average-calculator/
+//https://exceltable.com/formuly/formula-skolzyashchey-sredney?
+public class Sma {
+    public static void main(String[] args) {
+
+    }
+}
